5d66651a8c3a4f73377d5d094f4a17dd95ef8a92
WaerWaters <56149103+WaerWaters@users.noreply.github.com>
2022-02-27 14:47:01 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+let player;
+let bulletsFired = []
+
+function setup() {
+  createCanvas(400, 400);
+  player = new Player();
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    if (player.currentDirection === 1) {
+      bullet = new Bullet(player.x + 10, player.y + 10, -1, 0)
+      bulletsFired.push(bullet)
+    } else if (player.currentDirection === 2) {
+      bullet = new Bullet(player.x + 10, player.y + 10, 1, 0)
+      bulletsFired.push(bullet)
+    } else if (player.currentDirection === 3) {
+      bullet = new Bullet(player.x + 10, player.y + 10, 0, -1)
+      bulletsFired.push(bullet)
+    } else if (player.currentDirection === 4) {
+      bullet = new Bullet(player.x + 10, player.y + 10, 0, 1)
+      bulletsFired.push(bullet)
+    }
+  }
+}
+
+function draw() {
+  background(220);
+  player.show()
+  player.movement()
+  for (let i = 0; i < bulletsFired.length; i++) {
+    bulletsFired[i].show()
+    bulletsFired[i].update()
+    if (bulletsFired[i].outOfBounds()) {
+      bulletsFired.splice(i, 1)
+    }
+  }
+}
+
