fe960dc070213f0989988a202930392bad712acf
Drypperen <90191382+Drypperen@users.noreply.github.com>
2022-03-29 08:19:04 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,24 @@
 let player;
-let bulletsFired = []
+let zombieSpawned = [];
+let bulletsFired = [];
+
+
 
 function setup() {
   createCanvas(400, 400);
   player = new Player();
+  for (let i = 0; i < 10; i++) {
+    rand = Math.floor(Math.random() * 4)
+    if (rand == 0) {
+      zombieSpawned[i] = new Zombie(width/2, 20);
+    } else if (rand == 1) {
+      zombieSpawned[i] = new Zombie(20, height/2);
+    } else if (rand == 2) {
+      zombieSpawned[i] = new Zombie(width/2, height - 40);
+    } else if (rand == 3) {
+      zombieSpawned[i] = new Zombie(width - 40, height/2);
+    }
+  }
 }
 
 function keyPressed() {
@@ -24,10 +39,15 @@ function keyPressed() {
   }
 }
 
+
 function draw() {
   background(220);
   player.show()
   player.movement()
+  for(let i = 0; i < zombieSpawned.length; i++) {
+    zombieSpawned[i].update();
+  }
+  
   for (let i = 0; i < bulletsFired.length; i++) {
     bulletsFired[i].show()
     bulletsFired[i].update()
@@ -35,5 +55,42 @@ function draw() {
       bulletsFired.splice(i, 1)
     }
   }
+  
+  for (let i = 0; i < zombieSpawned.length; i++) {
+    zombieSpawned[i].show();
+
+    
+
+    /*let v0 = createVector(zombieSpawned[i].x,zombieSpawned[i].y);
+    let v1 = createVector(0-player.x,0-player.y);
+    v1.normalize();
+    console.log(v1);
+    console.log(v0);
+    if(v1.normalize().x > -0.25 && v1.normalize().x < 0.25 && v1.normalize().y < -0.75) {
+      zombieSpawned[i].y += 1
+    } /*else if(angleBetween < 1.125 && angleBetween > 0.375) {
+        zombieSpawned[i].x = zombieSpawned[i].x + 5
+        zombieSpawned[i].y = zombieSpawned[i].y - 5
+    } else if(angleBetween < 1.875 && angleBetween > 1.125) {
+        zombieSpawned[i].y = zombieSpawned[i].y - 5
+    } else if(angleBetween < 1.875 && angleBetween > 1.125) {
+        zombieSpawned[i].y = zombieSpawned[i].y - 5
+    } else if(angleBetween < 2.625 && angleBetween > 1.875) {
+        zombieSpawned[i].x = zombieSpawned[i].x - 5
+        zombieSpawned[i].y = zombieSpawned[i].y - 5
+    } else if(angleBetween < -2.625 && angleBetween > 2.625) {
+        zombieSpawned[i].x = zombieSpawned[i].x - 5
+    } else if(angleBetween < -1.875 && angleBetween > -2.625) {
+        zombieSpawned[i].x = zombieSpawned[i].x - 5
+        zombieSpawned[i].y = zombieSpawned[i].y + 5
+    } else if(angleBetween < -1.125 && angleBetween > -1.875) {
+        zombieSpawned[i].y = zombieSpawned[i].y + 5
+    } else if(angleBetween < -1.125 && angleBetween > -0.375) {
+        zombieSpawned[i].x = zombieSpawned[i].x + 5
+        zombieSpawned[i].y = zombieSpawned[i].y + 5
+    }*/
+  }
+  
 }
+
 
--- zombie.js
+++ zombie.js
@@ -1,4 +1,36 @@
 class Zombie{
- constructor()
-  
-}
+    constructor(x,y,damage,health,speed) {
+     this.x = x;
+     this.y = y;
+     this.width = 20;
+     this.height = 20;
+     this.damage = damage;
+     this.health = health;
+     this.speed = speed;
+     this.playerPosX = player.x;
+     this.playerPosY = player.y;
+   
+    }
+   show() {
+    fill(0,255,0);
+    rect(this.x,this.y,this.width,this.height);
+    
+    
+   }
+   update() {
+    let d = dist(this.x,this.y,player.x,player.y)
+    /*if (d<16) {
+        
+      }*/
+      let vx = (player.x-this.x)/d;
+      let vy = (player.y-this.y)/d;
+      this.x+=vx;
+      this.y+=vy;  
+      print(this.x);
+      print(this.y);
+   
+   
+   }
+   
+   }
+   
